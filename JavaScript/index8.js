4f4ea2a9ad5577c88957e15a9b8e1377eb9dfdcf
BrandonHillbom <bahillbom@gmail.com>
2020-07-23 11:21:00 -0600

    finished exercise

---
+++ JavaScript/index8.js
@@ -0,0 +1,11 @@
+const firstName = "Brandon";
+const lastName = "Hillbom";
+const age = 19;
+const dateOfBirth = "06/16/2001";
+const address = "4446 Castle Road";
+
+console.log("Name:", firstName);
+console.log("Surname:", lastName);
+console.log("Age:", age);
+console.log("Birth Date:", dateOfBirth);
+console.log("Address:", address);
