bec384d1c08960665621583bb73ca8b1a0010d4e
BrandonHillbom <bahillbom@gmail.com>
2020-07-23 21:54:17 -0600

    exercise completed

---
+++ JavaScript/index21.js
@@ -0,0 +1,7 @@
+let age = 19;
+let phone = 3065278888;
+let streetName = "Castle rd.";
+let streetNumber = 4488;
+console.log(`I'm ${age} years old`);
+console.log(`My phone number is: ${phone}`);
+console.log(`I live on ${streetNumber} ${streetName}`);
