adbcaab428abab7aa39e2aa34e03393ee21761d3
BrandonHillbom <bahillbom@gmail.com>
2020-07-23 21:52:57 -0600

    exercise completed

---
+++ JavaScript/index17.js
@@ -0,0 +1,7 @@
+let dragon1 = "Drogon";
+let dragon2 = "Rhaegal";
+let dragon3 = "Viserion";
+let seriesName = "Game of Thrones"
+
+let message = `In ${seriesName}, the dragon\'s names are: ${dragon1}, ${dragon2}, & ${dragon3}`;
+console.log(message);
